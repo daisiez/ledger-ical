edc2e24227fa68cd10161b2a9f40bb52a7b39e25
Eric Betts <bettse@gmail.com>
2011-06-05 18:47:11 -0700

    new php file to produce ical events of my daily ledger transactions.  untested

---
+++ xacts.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once( 'iCalcreator.class.php' );
+
+$ledger = "/usr/local/bin/ledger -f /Users/bettse/Dropbox/Finances/current.lgr";
+$daily_balance = ' -F "%(date)\t%(payee)\t%(amount)\n" -c -d "d > [today]-14"  --sort d reg "FirstTech:Checking"';
+$output = "";
+exec("$ledger $daily_balance", $output);
+
+$v = new vcalendar();
+// create a new calendar instance
+$v->setConfig( 'unique_id', 'ataraxia.ericbetts.org' );
+// set Your unique id
+
+$v->setProperty( 'method', 'PUBLISH' );
+// required of some calendar software
+$v->setProperty( "x-wr-calname", "Checking Balance" );
+// required of some calendar software
+$v->setProperty( "X-WR-CALDESC", "Daily events of my checking account as reported by Ledger" );
+// required of some calendar software
+$v->setProperty( "X-WR-TIMEZONE", "US/Pacific" );
+// required of some calendar software
+
+
+foreach ($output as $line){
+    //make into key-value pairs
+    $tmp = explode("\t", $line);
+//    print_r($tmp);
+    $date = $tmp[0];
+    $event = $tmp[1] . "\t" . $tmp[2];
+
+    // add event to calendar
+    $vevent = new vevent();
+    $vevent->setProperty( 'dtstart', $date, array('VALUE' => 'DATE'));
+    // alt. date format, now for an all-day event
+    $vevent->setProperty( 'summary', $event );
+    $v->setComponent ( $vevent );
+}
+
+$v->returnCalendar();
+// redirect calendar file to browser
+
+?>
+
+
